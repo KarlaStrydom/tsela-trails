69664e3a73f76897500feb9e6695f5d3a7c8ecc5
Stefan-Tolken <stefan@tolken.io>
2025-08-01 20:30:05 +0200

    Fixed build error

---
+++ tsela-trails/src/app/(frontend)/info/[slug]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default async function Info() {
+
+  return (
+    <div className="home">
+      <div className="content">
+        <div className="text-sm font-bold underline">
+          Info page
+        </div>
+      </div>
+    </div>
+  )
+}
